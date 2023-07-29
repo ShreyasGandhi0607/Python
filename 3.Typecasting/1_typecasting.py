0e48075198bb9703b789ae03839d4cc83c07f61b
Shreyas Gandhi <100945644+ShreyasGandhi0607@users.noreply.github.com>
2023-07-29 12:40:02 +0530

    Add files via upload
    typecasting

---
+++ 3.Typecasting/1_typecasting.py
@@ -0,0 +1,11 @@
+# for example 
+a = "1" #This is a string 
+# a = 1
+b = "2"  #This is a string 
+# b = 2
+# print(a+b)
+print(int(a)+int(b))
+
+# Typecasting
+# Python supports variety of functions or methods like eg. int() , float(), str(), ord(), hex(), oct(), tuple(), 
+# set(), list() , dict() etc. for tyoecasting in python
