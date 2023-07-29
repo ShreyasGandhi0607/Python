4c3c540772764531fdc775939247d007907ea9d5
Shreyas Gandhi <100945644+ShreyasGandhi0607@users.noreply.github.com>
2023-07-29 23:44:08 +0530

    Add files via upload

---
+++ 4.Strings/1_string.py
@@ -0,0 +1,27 @@
+name = "Shreyas"
+print("Hello " + name)
+
+apple = "Hey , I want to have some apples !"
+print(apple)
+
+sentence = '''
+Hey , I want to have some apples !
+Hey , I want to have some mangos !
+Hey , I want to have some bananas !
+'''
+
+
+print(sentence)
+
+# INDEXING
+
+print(name[0])
+print(name[1])
+print(name[2])
+# for print(name[8]) it throws an index error
+
+print("Lets use  a for loop")
+
+for characters in apple:
+    print(characters)
+    
