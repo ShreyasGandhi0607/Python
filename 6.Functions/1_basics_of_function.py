a75269313644930a5f4f09b40f54d0ae95b51fff
Shreyas <shreyasgandhi673@gmail.com>
2023-07-31 22:13:51 +0530

    function

---
+++ 6.Functions/1_basics_of_function.py
@@ -0,0 +1,33 @@
+a = 9
+b = 8
+def calGmean(a,b):
+    gmean = (a*b)/(a+b)
+    print(gmean)
+calGmean(a,b)
+c = 8
+d = 7
+# gmean1 = (c*d)/(c+d)
+# print(gmean1)
+def isGreater(a,b):
+    if (a>b):
+        print("First number is greater ")
+    else:
+        print("Second number is greater  or equal")
+
+def isLesser(a,b):
+    pass #this can be written afterwords
+
+calGmean(c,d)
+
+isGreater(a,b)
+isGreater(c,d)
+
+# Two types of functions 
+# Built in function 
+# User defined function
+print(__name__)
+print(__builtins__)
+
+
+
+
