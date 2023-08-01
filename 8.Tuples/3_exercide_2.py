4865c682e7958f41667cf1535d0b355ca69e4f13
Shreyas <shreyasgandhi673@gmail.com>
2023-08-01 15:39:45 +0530

    main

---
+++ 8.Tuples/3_exercide_2.py
@@ -0,0 +1,4 @@
+# create a program capable of displaying questions to the user like KBC.
+# use list data type to store their questions and their correct answers
+# Displaying the final amount the person is taking home 
+
