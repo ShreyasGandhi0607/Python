8354d0ea3b0a457b289d1fce8ad7ab01f6ed678b
Shreyas Gandhi <100945644+ShreyasGandhi0607@users.noreply.github.com>
2023-08-01 22:21:29 +0530

    Add files via upload

---
+++ 11.Sets/1_sets_basics.py
@@ -0,0 +1,22 @@
+# set is a collection of well defined objects
+# sets are unordered collection of data items
+# sets are mutable
+# sets are unindexed
+# sets are represented by {}
+# sets are not allow duplicate values
+s = {2,4, 2 ,6 }
+print(s)
+
+info = {"Carla ", 19 , True , 5.8}
+print(info)
+
+# set() function
+# set() function is used to create a set
+# set() function is used to convert other data types to set
+# set() function is used to remove duplicate values
+
+# shrey = {}#it gives empty dictionery
+# print(type(shrey))
+
+shrey = set()#it gives empty set
+print(type(shrey))
