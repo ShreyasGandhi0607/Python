a77f4208f78dd28b35da6ff58e84ce2770c01eb4
Shreyas <shreyasgandhi673@gmail.com>
2023-08-05 11:46:47 +0530

    file handling

--- 15.FileHandling/main.py
+++ 15.FileHandling/main.py
@@ -13,3 +13,39 @@ f.close()
 
 with open('myfile.txt', 'a') as f:
     f.write("Hey I am inside with")
+
+
+# READING A FILE
+# LINE BY LINE
+f = open('myfile.txt', 'r')
+for line in f:
+    print(line)
+f.close()
+
+# ALL AT ONCE
+f = open('myfile.txt', 'r')
+text = f.read()
+print(text)
+f.close()
+
+# methods in file 
+# f.read()
+# f.readline()
+# f.readlines()
+# f.write()
+# f.close()
+# f.tell()
+# f.seek()
+# f.closed
+# f.mode
+# f.name
+# f.encoding
+# f.errors
+# f.fileno()
+# f.flush()
+# f.isatty()
+# f.line_buffering()
+# f.newlines
+
+
+        
