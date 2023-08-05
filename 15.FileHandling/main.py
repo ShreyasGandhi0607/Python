2f2dde28fce918c350a7cac51bd0beacd24321d0
Shreyas <shreyasgandhi673@gmail.com>
2023-08-05 11:25:58 +0530

    file handling

---
+++ 15.FileHandling/main.py
@@ -0,0 +1,15 @@
+f = open("requirement.txt", 'r')
+text  = f.read()
+print(text )
+
+
+f.close()
+
+# WRITING A FILE
+
+f = open('myfile.txt', 'a')
+f.write('Hello, world!')
+f.close()
+
+with open('myfile.txt', 'a') as f:
+    f.write("Hey I am inside with")
